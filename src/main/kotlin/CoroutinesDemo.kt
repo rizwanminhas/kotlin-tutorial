db03237ea5ba282dccd2974c57a9a4ac4ba6426b
rizwan <rizwan@rizwans-MBP.fios-router.home>
2023-01-13 18:20:02 -0500

    job and deferred examples

--- src/main/kotlin/CoroutinesDemo.kt
+++ src/main/kotlin/CoroutinesDemo.kt
@@ -1,6 +1,7 @@
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlinx.coroutines.*
+import kotlin.math.log
 
 val logger: Logger = LoggerFactory.getLogger("CoroutinesDemo")
 
@@ -48,10 +49,67 @@ suspend fun noStructConcurrencyMorningRoutine() {
     GlobalScope.launch { bathTime() }
     GlobalScope.launch { boilingWatter() }
 }
+
+suspend fun makeCoffee() {
+    logger.info("starting to make coffee")
+    delay(500)
+    logger.info("coffee is ready")
+}
+
+suspend fun morningRoutineWithCoffee() {
+    coroutineScope {
+        // job gives you control flow, launch 2 tasks in parallel and then wiat for both of them to complete before starting the 3rd task in parallel
+        val bathTimeJob: Job = launch { bathTime() }
+        val boilingWaterJob: Job = launch { boilingWatter() }
+        bathTimeJob.join()
+        boilingWaterJob.join()
+        launch { makeCoffee() }
+    }
+}
+
+// same as morningRoutineWithCoffee but more clean
+suspend fun morningRoutineWithCoffeeStructured() {
+    coroutineScope {
+        coroutineScope {
+            launch { bathTime() }
+            launch { boilingWatter() }
+        }
+        launch { makeCoffee() }
+    }
+}
+
+// return values from coroutines
+suspend fun preparingJavaCoffee(): String {
+    logger.info("starting to make java coffee")
+    delay(500)
+    logger.info("java coffee is ready")
+    return "Java coffee"
+}
+
+suspend fun toastingBread(): String {
+    logger.info("starting to make breakfast")
+    delay(1000)
+    logger.info("toast is ready")
+    return "Toasted bread"
+}
+
+suspend fun prepareBreakfast() {
+    coroutineScope {
+        val coffee = async { preparingJavaCoffee() } // Deferred = analogous of Future[T] in scala
+        val toast = async { toastingBread() }
+        // semantic blocking
+        val finalCoffee = coffee.await()
+        val finalToast = toast.await()
+        logger.info("I am eating $finalToast and drinking $finalCoffee")
+    }
+}
+
 suspend fun main(array: Array<String>) {
 //bathTime()
 //sequentialMorningRoutine()
 // concurrentMorningRoutine()
-    noStructConcurrencyMorningRoutine()
-    delay(2000) // needed for noStructConcurrencyMorningRoutine
+    //noStructConcurrencyMorningRoutine()
+    //delay(2000) // needed for noStructConcurrencyMorningRoutine
+
+    prepareBreakfast()
 }
